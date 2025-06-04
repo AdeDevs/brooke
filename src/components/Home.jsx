49d060cfa4703ea23ae83ef18b0a477f12802e78
AdeDevs <adeyemiakinyemi01@gmail.com>
2025-06-04 23:27:49 +0100

    updt

---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+import { NavLink } from "react-router-dom"
+
+export default function HomePage() {
+    return (
+        <div className="home-page">
+            <h1>koninchiwa brooke san</h1>
+        </div>
+    )
+}
---
+++ src/components/Routes.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react"
+import { NavLink } from "react-router-dom"
+
+export default function NavBar() {
+    const [showMenu, setShowMenu] = useState(false)
+    const toggleMenu = () => {
+        setShowMenu(!showMenu)
+    }
+    useEffect(() => {
+        if(showMenu) {
+            document.body.classList.add('no-scroll')
+        } else {
+            document.body.classList.remove('no-scroll')
+        }
+    }, [showMenu])
+    return (
+        <div className="nav-bar">
+            <NavLink to="/"><h1 className="logo">be</h1></NavLink>
+            <ul className="desk-nav">
+                <li><NavLink to="/">about <br /> me</NavLink></li>
+                <li><a href="#about">highlight <br /> career</a></li>
+                <li><a href="#services">Partner & <br /> events</a></li>
+                <li><a href="#contact">contact <br /> me</a></li>
+            </ul>
+            <ul className="desk-nav extras">
+                <li>Nike</li>
+            <div className={`toggle ${showMenu ? "show" : ""}`} onClick={toggleMenu}>
+                <span className={`first`}></span>
+                <span className={`second`}></span>
+            </div>
+            </ul>
+            <ul className={`mobile-nav ${showMenu ? "show" : ""}`}>
+                <li onClick={toggleMenu}><NavLink to="/">about me</NavLink></li>
+                <li onClick={toggleMenu}><a href="#about">highlight career</a></li>
+                <li onClick={toggleMenu}><a href="#services">Partner & events</a></li>
+                <li onClick={toggleMenu}><a href="#contact">contact <br /> me</a></li>
+            </ul>
+        </div>
+    )
+}
